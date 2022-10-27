cbc9d908a9b77a86a970c0c682e28a1cd1f362f6
Darwin 21.5.0 <111538309+ian42069@users.noreply.github.com>
2022-10-27 14:28:56 -0700

    wip 3

--- Turtle/smiley.py
+++ Turtle/smiley.py
@@ -10,36 +10,6 @@ window.colormode(255)
 smiley = turtle.Turtle()
 window.tracer(0)
 
-
-def square(x, y, side, pensize=1, color="black", fill_color="", angle=0):
-    # make the turtle face the angle they specify
-    smiley.setheading(angle)
-
-    smiley.pensize(pensize)
-    smiley.pencolor(color)
-    smiley.penup()
-    smiley.goto(x, y)
-    smiley.pendown()
-
-    # check if they chose a custom fill_color
-    # fill_color would be something other than ""
-    if fill_color != "":  # != means it is true if the two are different
-        smiley.fillcolor(fill_color)
-        smiley.begin_fill()
-
-    smiley.forward(side)
-    smiley.right(90)
-    smiley.forward(side)
-    smiley.right(90)
-    smiley.forward(side)
-    smiley.right(90)
-    smiley.forward(side)
-    smiley.right(90)
-
-    if fill_color != "":
-        smiley.end_fill()
-
-
 def circle(x, y, radius, pensize=1, color="black", fill_color=""):
     # make the turtle face the angle they specify
     smiley.setheading(0)
@@ -63,7 +33,7 @@ def circle(x, y, radius, pensize=1, color="black", fill_color=""):
 
 
 # all the code to draw the picture goes here
-square(20, 50, 100, color="red", fill_color="black")
 circle(20, 50, 150, color="yellow", fill_color="yellow")
+circle(-60, 100, 20, color="black", fill_color="black")
 
 window.mainloop()
