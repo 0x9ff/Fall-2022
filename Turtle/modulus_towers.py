f27cd90fc2acf8ffde5ba934455de628bbacd2cb
Darwin 21.5.0 <111538309+ian42069@users.noreply.github.com>
2022-11-09 13:14:00 -0800

    start modulus_towers.py

---
+++ Turtle/modulus_towers.py
@@ -0,0 +1,69 @@
+######################## ASSIGNMENT #############################
+# Put your code for each part below the related comment         #
+# 1. make the tower rotate through 3 colors all the way up      #
+# 2. make a 2nd and 3rd tower to the right of the first that    #
+#    also rotates through 4 and 5 colors respectively.          #
+# 3. Make a 4th tower to the right of the others. However,      #
+# this one is a pyramid of random colors.                       #
+# Hint: in the loop, shorten the width and adjust the start     #
+# location                                                      #
+#                                                               #
+# BONUS:                                                        #
+# 1. Make the buildings have an outline (1 pixel of a different #
+# color all the way around them).                               #
+# 2. Make an Hourglass. (think pyramid, but with an inverse     #
+# pyramid on top of it)  Give it an outline. Make it look like  #
+# sand is dripping out of it and.                               #
+#                                                               #
+#               #########                                       #
+#                #     #                                        #
+#                 #...#                                         #
+#                  #.#                                          #
+#                   #                                           #
+#                  #.#                                          #
+#                 # . #                                         #
+#                #.....#                                        #
+#               #########                                       #
+#                                                               #
+#################################################################
+
+# modulus_towers.py
+# last modified 11/09/2022
+# created 11/09/2022
+
+import turtle as trtl
+
+wn = trtl.Screen()
+wn.colormode(255)
+
+painter = trtl.Turtle()
+painter.speed(0)
+painter.pensize(5)
+
+# starting location and dimensions of the tower
+x = -500
+y = -150
+width = 100
+num_floors = 50
+
+# 1. Tower 1
+for floor in range(num_floors):
+    # set placement and color of turtle
+    painter.penup()
+    painter.goto(x, y)
+    painter.color("gray")
+    y = y + 5  # location of next floor
+
+    # draw the floor
+    painter.pendown()
+    painter.forward(width)
+
+# 2. Tower 2
+
+# 3. Tower 3
+
+# 4. Pyramid
+
+# Bonus Hourglass
+
+wn.mainloop()
